10fdc671afb9f5c4d1d833e00a9995f94403eea6
MDorizon <mdorizon@proton.me>
2024-12-18 17:25:39 +0100

    :sparkles: mot de passe crypté

--- api/src/routes/auth/auth.service.ts
+++ api/src/routes/auth/auth.service.ts
@@ -1,32 +1,28 @@
-import { Request, Response } from "express";
-import userService from "../user/user.service";
-import { IUser, IUserDTO, SigninIUserDTO } from "../user/user.types";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import userService from "../user/user.service";
+import { IUser, IUserDTO, SigninIUserDTO } from "../user/user.types";
+
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 const signin = async (userDTO: SigninIUserDTO) => {
-  // Check if user exists
   const user = await userService.getOneByEmail(userDTO.email);
-  console.log(user)
 
   if (!user) {
     return null;
   }
 
-  if (user.password !== userDTO.password) {
+  const isPasswordValid = await bcrypt.compare(userDTO.password, user.password);
+  if (!isPasswordValid) {
     return null;
   }
 
-  const access_token = jwt.sign(
-    { id: user.id, email: user.email },
-    JWT_SECRET,
-    {
-      expiresIn: "12h",
-    }
-  );
+  const access_token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
+    expiresIn: "12h",
+  });
 
   return access_token;
 };
@@ -36,7 +32,7 @@ const signup = async (userDTO: IUserDTO) => {
 };
 
 const whoami = async (user_id: number) => {
-  return await userService.getOneById(user_id)
+  return userService.getOneById(user_id);
 };
 
 export default {
--- api/src/routes/user/user.service.ts
+++ api/src/routes/user/user.service.ts
@@ -1,15 +1,17 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import client from "../../config/database.config";
 import { IUser, IUserDTO } from "./user.types";
 
+const SALT_ROUNDS = 10;
+
 const getAll = async (req: Request, res: Response) => {
   try {
-    const result = await client.query('SELECT * FROM public.user');
-
-    res.status(200).send(result.rows);
+    const result = await client.query("SELECT id, username, email FROM public.user");
+    res.status(200).send(result.rows); // Exclusion du mot de passe
   } catch (e) {
-    console.error('Database error:', e);
-    res.status(500).send({ error: 'Error while fetching data' });
+    console.error("Database error:", e);
+    res.status(500).send({ error: "Error while fetching data" });
   }
 };
 
@@ -27,14 +29,13 @@ const getOneByEmail = async (email: string): Promise<IUser | null> => {
   return user;
 };
 
-const getOneById = async (id: number): Promise<IUser | null> => {
-  const query = "SELECT * FROM public.user WHERE id = $1";
+const getOneById = async (id: number): Promise<Partial<IUser> | null> => {
+  const query = "SELECT id, username, email FROM public.user WHERE id = $1"; // Exclusion du mot de passe
   const values = [id];
 
   try {
     const result = await client.query(query, values);
     const user = result.rows[0];
-
     return user;
   } catch (error) {
     console.error("Error fetching user:", error);
@@ -43,12 +44,12 @@ const getOneById = async (id: number): Promise<IUser | null> => {
 };
 
 const create = async (userDTO: IUserDTO) => {
+  const hashedPassword = await bcrypt.hash(userDTO.password, SALT_ROUNDS);
   const query = "INSERT INTO public.user (username, password, email) VALUES ($1, $2, $3)";
-  const values = [userDTO.username, userDTO.password, userDTO.email];
+  const values = [userDTO.username, hashedPassword, userDTO.email];
 
   try {
     await client.query(query, values);
-
     return true;
   } catch (error) {
     console.error("Error creating user:", error);
@@ -56,10 +57,10 @@ const create = async (userDTO: IUserDTO) => {
   }
 };
 
-const update = async(req: Request, res: Response) => {
+const update = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const result = (await client.query('SELECT * FROM public.user WHERE id = $1', [id])).rows;
+    const result = (await client.query("SELECT * FROM public.user WHERE id = $1", [id])).rows;
 
     if (Array.isArray(result) && result.length === 0) {
       res.status(404).send({ error: "User not found" });
@@ -73,47 +74,48 @@ const update = async(req: Request, res: Response) => {
         ...req.body,
       };
 
-      const sqlUpdate = 'UPDATE public.user SET username=$1, password=$2, email=$3 WHERE id = $4';
+      const sqlUpdate =
+        "UPDATE public.user SET username=$1, password=$2, email=$3 WHERE id = $4";
       const values = [
-        newUser.username, 
-        newUser.password,
+        newUser.username,
+        req.body.password ? await bcrypt.hash(newUser.password, SALT_ROUNDS) : currentUser.password,
         newUser.email,
-        id
-      ]
+        id,
+      ];
       try {
         await client.query(sqlUpdate, values);
         res.status(200).send({ message: "User updated successfully" });
       } catch (e) {
-        console.error('Update error:', e);
-        res.status(500).send({ error: 'Error while modifying data' });
+        console.error("Update error:", e);
+        res.status(500).send({ error: "Error while modifying data" });
       }
     }
   } catch (e) {
-    console.error('Database error:', e);
-    res.status(500).send({ error: 'Error while fetching data' });
+    console.error("Database error:", e);
+    res.status(500).send({ error: "Error while fetching data" });
   }
 };
 
-const remove = async(req: Request, res: Response) => {
+const remove = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-    const result = (await client.query('SELECT * FROM public.user WHERE id = $1', [id])).rows;
+    const result = (await client.query("SELECT * FROM public.user WHERE id = $1", [id])).rows;
     if (Array.isArray(result) && result.length === 0) {
       res.status(404).send({ error: "User not found" });
       return;
-    };
-    
+    }
+
     try {
-      await client.query('DELETE FROM public.user WHERE id = $1', [id]);
+      await client.query("DELETE FROM public.user WHERE id = $1", [id]);
       res.status(204).send();
     } catch (e) {
-      console.error('Database error:', e);
-      res.status(500).send({ error: 'Error while fetching data' });
+      console.error("Database error:", e);
+      res.status(500).send({ error: "Error while fetching data" });
     }
   } catch (e) {
-    console.error('Database error:', e);
-    res.status(500).send({ error: 'Error while fetching data' });
+    console.error("Database error:", e);
+    res.status(500).send({ error: "Error while fetching data" });
   }
 };
 
